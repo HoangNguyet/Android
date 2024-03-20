cab981973ad913ff524c3f3c93b7e1061a4ec3ef
HoangNguyet <nguyethoanghan@gmail.com>
2024-03-20 09:01:15 +0700

    update project

--- app/src/main/java/com/example/happy_read/activity/MainDangNhap.java
+++ app/src/main/java/com/example/happy_read/activity/MainDangNhap.java
@@ -2,14 +2,47 @@ package com.example.happy_read.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.happy_read.R;
+import com.example.happy_read.database.database;
 
 public class MainDangNhap extends AppCompatActivity {
+
+    private EditText edUserName, edPassword;
+    private Button btndangnhap, btndangki;
+    private database database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_dang_nhap);
+
+        // Khởi tạo các thành phần trong giao diện
+        edUserName = findViewById(R.id.ed_username);
+        edPassword = findViewById(R.id.ed_password);
+        btndangki = findViewById(R.id.btn_dangki);
+        btndangnhap = findViewById(R.id.btn_dangnhap);
+        database = new database(this);
+
+        //xử lý sự kiện khi người dùng nhấn nút "Đăng nhập"
+        btndangnhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = edUserName.getText().toString().trim();
+                String password = edPassword.getText().toString().trim();
+                //Kiểm tra xem người dùng đã nhập đầy đủ thông tin chưa
+                if (username.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(MainDangNhap.this, "Vui lòng nhập tên đăng nhập và mật khẩu", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
+        });
+
+        //xử lý sự kiện khi người dùng nhấn nút "Đăng ký"
+
     }
 }
--- app/src/main/java/com/example/happy_read/database/database.java
+++ app/src/main/java/com/example/happy_read/database/database.java
@@ -45,12 +45,6 @@ public class database extends SQLiteOpenHelper {
     public static final String COLUMN_RATINGS_COMMENT = "comment";
     public static final String COLUMN_RATINGS_ISFAVORITE = "isfavorite";
 
-    //Tên bảng và các cột của bảng favorite
-    public static final String TABLE_FAVORITE = "favorites";
-    public static final String COLUMN_FAVORITE_ID = "id";
-    public static final String COLUMN_FAVORITE_USER_ID = "user_id";
-    public static final String COLUMN_FAVORITE_STORY_ID = "story_id";
-
     // Câu lệnh tạo bảng user
     private static final String SQL_CREATE_TABLE_USERS =
             "CREATE TABLE " + TABLE_USERS + " (" +
@@ -95,56 +89,101 @@ public class database extends SQLiteOpenHelper {
                     "FOREIGN KEY (" + COLUMN_RATINGS_STORY_ID + ") REFERENCES " +
                     TABLE_STORIES + "(" + COLUMN_STORIES_ID + ")" +
                     ")";
-
-    //câu lệnh tạo bảng favorite
-    private static final String SQL_CREATE_TABLE_FAVORITE =
-            "CREATE TABLE " + TABLE_FAVORITE + " (" +
-                    COLUMN_FAVORITE_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                    COLUMN_FAVORITE_USER_ID + " INTEGER NOT NULL, " +
-                    COLUMN_FAVORITE_STORY_ID + " INTEGER NOT NULL, " +
-                    "FOREIGN KEY (" + COLUMN_FAVORITE_USER_ID + ") REFERENCES " +
-                    TABLE_USERS + "(" + COLUMN_USERS_ID + "), " +
-                    "FOREIGN KEY (" + COLUMN_FAVORITE_STORY_ID + ") REFERENCES " +
-                    TABLE_STORIES + "(" + COLUMN_STORIES_ID + ")" +
-                    ")";
-
     // Câu lệnh xóa bảng user
-    private String sql1 = "INSERT INTO users (username, password, email, fullname, userrole)\n" +
-            "VALUES ('admin', 'admin', 'admin@example.com', 'Hoàng Ánh Nguyệt', 'admin'),\n" +
-            "('anh', 'anh', 'jane@example.com', 'Nguyễn Thị Ngọc Ánh', 'admin'),\n" +
-            "('ninh', 'ninh', 'smith@example.com', 'Lê Khắc Ninh', 'user')";
+    private String sql1 = "INSERT INTO users (username, password, email, fullname, userrole, image, gender, birthday)\n" +
+            "VALUES ('admin', 'admin', 'admin@example.com', 'Hoàng Ánh Nguyệt', 'admin', 'drawable/avata1.webp', 'female', '2003-01-31'),\n" +
+            "('anh', 'anh', 'jane@example.com', 'Nguyễn Thị Ngọc Ánh', 'admin', 'drawable/avata1.webp', 'female', '2003-01-31'),\n" +
+            "('ninh', 'ninh', 'smith@example.com', 'Lê Khắc Ninh', 'user', 'drawable/avata1.webp', 'male', '2003-01-14')";
 
 
-    private String SQLQuery4 = "INSERT INTO truyen VALUES (null,'Doraemon', Mùa đông đã đến rồi trời lạnh buốt, Doraemon không có gì để ăn cả. Doraemon mặc áo vào rồi ra ngoài kiếm thức ăn. Nó đi mãi đi mãi cuối cùng cũng tìm được hai củ cải trắng. Doraemon reo lên:\n" +
-            "\n" +
-            "– Ôi, ở đây có hai củ cải trắng liền, mình thật là may mắn!\n" +
-            "\n" +
-            "Doraemon đói bụng, muốn ăn lắm rồi. Nhưng Doraemon lại nghĩ:\n" +
-            "\n" +
-            "– Ừm… trời lạnh thế này, chắc Nobita cũng không có cái gì để ăn đâu. Mình phải mang cho Nobita một củ mới được.\n" +
-            "\n" +
-            "Thế là Doraemon đi sang nhà bạn Nobita nhưng Nobita không có nhà nên Doraemon đặt củ cải lên bàn rồi đi về.\n" +
-            "\n" +
-            "Tình cờ, Nobita con đi chơi cũng tìm được một củ cải trắng nhưng nó chỉ ăn trước một nửa.\n" +
-            "\n" +
-            "Về đến nhà, lại thấy có một củ cải trắng ở trên bàn Nobita thèm ăn lắm, nhưng lại nghĩ:\n" +
-            "\n" +
-            "– Ôi trời lạnh thế này chắc Shizuka không có cái gì để ăn rồi, mình phải mang cho Shizuka mới được.\n" +
-            "\n" +
-            "Nobita con đến nhà Shizuka nhưng Shizuka lại đi vắng, Nobita bèn đặt củ cải ở trên bàn rồi về.\n" +
-            "\n" +
-            "Khi Shizuka về nhà, thấy củ cải ở trên bàn, Shizuka ngạc nhiên lắm.\n" +
-            "\n" +
-            "– Ồ, củ cải trắng ở đâu mà ngon vậy nhỉ. Xuỵt… thích quá. Nhưng chắc trời lạnh thế này, Doraemon cũng không có gì ăn đâu. Mình phải mang sang cho Doraemon mới được.\n" +
-            "\n" +
-            "Khi Shizuka đến thì Doraemon đang ngủ rất say. Khi tỉnh dậy Doraemon lại thấy trên bàn mình xuất hiện một củ cải trắng.Doraemon vui lắm nó chạy đi gọi các bạn:\n" +
-            "\n" +
-            "– Bạn Shizuka ơi, bạn Nobita ơi hãy đến nhà tôi, chúng ta cùng ăn củ cải trắng thơm ngon này.\n" +
-            "\n" +
-            "Thế là cuối cùng, củ cải trắng ấy được chia sẻ cho cả ba người bạn tốt bụng của chúng ta. Các bạn thấy đấy tấm lòng thơm thảo, sẵn sàng sẻ chia của các bạn ấy thật là đáng học tập phải không nào?\n" +
-            "\n" +
-            "Ý nghĩa câu chuyện: Khi cho đi bạn sẽ nhận lại được nhiều hơn những thứ mình có.','Thiếu nhi', '2023.10.24', '2023.11.14', '', 'drawable/doraemon.jpg',1)";
+    private String sql2 = "INSERT INTO truyen VALUES (null, " +
+            "'Doraemon', " +
+            "'Mùa đông đã đến rồi trời lạnh buốt, Doraemon không có gì để ăn cả." +
+            " Doraemon mặc áo vào rồi ra ngoài kiếm thức ăn. Nó đi mãi đi mãi cuối cùng cũng tìm được hai củ cải trắng. " +
+            "Doraemon reo lên: Ôi, ở đây có hai củ cải trắng liền, mình thật là may mắn! Doraemon đói bụng, muốn ăn lắm rồi. " +
+            "Nhưng Doraemon lại nghĩ: Ừm… trời lạnh thế này, chắc Nobita cũng không có cái gì để ăn đâu." +
+            " Mình phải mang cho Nobita một củ mới được. Thế là Doraemon đi sang nhà bạn Nobita nhưng Nobita không có nhà nên Doraemon đặt củ cải lên bàn rồi đi về. " +
+            "Tình cờ, Nobita con đi chơi cũng tìm được một củ cải trắng nhưng nó chỉ ăn trước một nửa." +
+            " Về đến nhà, lại thấy có một củ cải trắng ở trên bàn Nobita thèm ăn lắm, nhưng lại nghĩ:" +
+            " Ôi trời lạnh thế này chắc Shizuka không có cái gì để ăn rồi, mình phải mang cho Shizuka mới được." +
+            " Nobita con đến nhà Shizuka nhưng Shizuka lại đi vắng, Nobita bèn đặt củ cải ở trên bàn rồi về. " +
+            "Khi Shizuka về nhà, thấy củ cải ở trên bàn, Shizuka ngạc nhiên lắm. Ồ, củ cải trắng ở đâu mà ngon vậy nhỉ." +
+            " Xuỵt… thích quá. Nhưng chắc trời lạnh thế này, Doraemon cũng không có gì ăn đâu. Mình phải mang sang cho Doraemon mới được. " +
+            "Khi Shizuka đến thì Doraemon đang ngủ rất say. Khi tỉnh dậy Doraemon lại thấy trên bàn mình xuất hiện một củ cải trắng." +
+            " Doraemon vui lắm nó chạy đi gọi các bạn: Bạn Shizuka ơi, bạn Nobita ơi hãy đến nhà tôi, chúng ta cùng ăn củ cải trắng thơm ngon này. " +
+            "Thế là cuối cùng, củ cải trắng ấy được chia sẻ cho cả ba người bạn tốt bụng của chúng ta. Các bạn thấy đấy tấm lòng thơm thảo," +
+            " sẵn sàng sẻ chia của các bạn ấy thật là đáng học tập phải không nào?', " +
+            "'Thiếu nhi', " +
+            "'2023-10-24', " +
+            "'2023-11-14', " +
+            "'drawable/doraemon.jpg', " +
+            "3101, " +
+            "1)";
+    private String sql3 = "INSERT INTO truyen VALUES (null, " +
+            "'Conan', " +
+            "'Conan là một nhân vật trong truyện tranh nổi tiếng của Nhật Bản. Cậu bé này được biết đến với tên thật là Shinichi Kudo, " +
+            "một học sinh cấp 3 tài năng và nổi tiếng trong việc phá án. " +
+            "Trong một lần tình cờ, Shinichi bị biến thành một cậu bé nhỏ tuổi hơn sau khi bị độc thuốc của tổ chức Áo Đen." +
+            " Với sự trợ giúp của bác sĩ Agasa, Shinichi sống dưới danh tính Conan Edogawa và tiếp tục phá án để tìm ra tung tích của tổ chức Áo Đen.', " +
+            "'Hành động', " +
+            "'2023-09-10', " +
+            "'2023-09-25', " +
+            "'drawable/conan.jpg', " +
+            "1500, " +
+            "2)";
 
+    private String sql4 = "INSERT INTO truyen VALUES (null, " +
+            "'One Piece', " +
+            "'One Piece là một bộ truyện tranh nổi tiếng của tác giả Eiichiro Oda." +
+            " Câu chuyện kể về cuộc hành trình của Monkey D. Luffy - một cậu bé trẻ đam mê trở thành Vua Hải Tặc và tìm kiếm kho báu One Piece để trở thành Vua Hải Tặc." +
+            " Cùng với băng hải tặc Mũ Rơm của mình, Luffy vượt qua nhiều thử thách và gặp gỡ nhiều nhân vật đầy màu sắc trên đường đi.', " +
+            "'Phiêu lưu', " +
+            "'2023-08-05', " +
+            "'2023-08-20', " +
+            "'drawable/one_piece.jpg', " +
+            "2500, " +
+            "1)";
+
+    private String sql5 = "INSERT INTO truyen VALUES (null, " +
+            "'Naruto', " +
+            "'Naruto là một bộ truyện tranh kinh điển của tác giả Masashi Kishimoto. " +
+            "Câu chuyện xoay quanh cuộc phiêu lưu của Naruto Uzumaki - một cậu bé có ước mơ trở thành Hokage, người lãnh đạo của làng ninja. " +
+            "Trong hành trình của mình, Naruto phải đối mặt với nhiều thử thách và học được nhiều bài học quý giá về tình bạn, lòng dũng cảm và sự kiên nhẫn.', " +
+            "'Hành động, phiêu lưu', " +
+            "'2023-07-15', " +
+            "'2023-07-30', " +
+            "'drawable/naruto.jpg', " +
+            "2000, " +
+            "1)";
+
+    private String sql6 = "INSERT INTO truyen VALUES (null, " +
+            "'Dragon Ball', " +
+            "'Dragon Ball là một trong những bộ truyện tranh huyền thoại của Nhật Bản, sáng tạo bởi Akira Toriyama. " +
+            "Câu chuyện xoay quanh cuộc hành trình của Goku - một chiến binh Siêu Saya và các đồng đội của mình trong việc tìm kiếm các viên ngọc rồng và " +
+            "chiến đấu chống lại các kẻ thù đáng sợ như Frieza, Cell và Buu để bảo vệ trái đất.', " +
+            "'Hành động, võ thuật', " +
+            "'2023-06-20', " +
+            "'2023-07-05', " +
+            "'drawable/dragon_ball.jpg', " +
+            "1800, " +
+            "2)";
+
+    private String sql7 = "INSERT INTO truyen VALUES (null, " +
+            "'Attack on Titan', " +
+            "'Attack on Titan (tên tiếng Nhật: Shingeki no Kyojin) là một bộ truyện tranh nổi tiếng của tác giả Hajime Isayama. " +
+            "Câu chuyện diễn ra trong một thế giới nơi con người sống trong những thành trì khổng lồ để tránh khỏi cuộc tấn công " +
+            "của những sinh vật khổng lồ có tên là Titan. Eren Yeager và những người bạn của cậu gia nhập quân đội để chiến đấu chống lại Titan và " +
+            "khám phá bí mật về thế giới của họ.', " +
+            "'Hành động, kinh dị', " +
+            "'2023-05-10', " +
+            "'2023-05-25', " +
+            "'drawable/attack_on_titan.jpg', " +
+            "2200, " +
+            "1)";
+
+    private String sql8 = "INSERT INTO ratings VALUES (1, 1, 5, 'Truyện rất hay!', 1)";
+    private String sql9 = "INSERT INTO ratings VALUES (2, 2, 4, 'Cốt truyện hấp dẫn!', 0)";
+    private String sql10 = "INSERT INTO ratings VALUES (3, 3, 3, 'Truyện khá thú vị.', 1)";
 
     public database(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -158,6 +197,15 @@ public class database extends SQLiteOpenHelper {
         db.execSQL(SQL_CREATE_TABLE_STORIES);
         db.execSQL(SQL_CREATE_TABLE_RATINGS);
         db.execSQL(sql1);
+        db.execSQL(sql2);
+        db.execSQL(sql3);
+        db.execSQL(sql4);
+        db.execSQL(sql5);
+        db.execSQL(sql6);
+        db.execSQL(sql7);
+        db.execSQL(sql8);
+        db.execSQL(sql9);
+        db.execSQL(sql10);
     }
 
     @Override
