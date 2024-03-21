85678c7ed2a410137c8be087e1c0619b9d37d5f3
hihiha <khacninh2020@gmail.com>
2024-03-21 17:56:38 +0700

    interface: controls profile and edit profile

--- app/src/main/java/com/example/happy_read/activity/UserProfileActivity.java
+++ app/src/main/java/com/example/happy_read/activity/UserProfileActivity.java
@@ -1,5 +1,24 @@
 package com.example.happy_read.activity;
 
-public class UserProfileActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.happy_read.R;
+
+public class UserProfileActivity extends AppCompatActivity {
 //    Hiển thị thông tin người dùng và truyện mà họ đã đọc và yêu thích
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+    public void EditProFile(View v){
+        Intent intent = new Intent(UserProfileActivity.this, UserProfileEditActivity.class);
+        startActivity(intent);
+    }
 }
