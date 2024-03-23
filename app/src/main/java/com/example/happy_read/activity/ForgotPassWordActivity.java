bf825cc7940dc6d6cfd9132f7db0e1f778b6daa7
HoangNguyet <nguyethoanghan@gmail.com>
2024-03-23 21:42:33 +0700

    up

---
+++ app/src/main/java/com/example/happy_read/activity/ForgotPassWordActivity.java
@@ -0,0 +1,15 @@
+package com.example.happy_read.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.happy_read.R;
+
+public class ForgotPassWordActivity  extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quenmatkhau);
+    }
+}
