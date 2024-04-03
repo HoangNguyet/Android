96c1b90ea41dd4064a5ecb03ec0b9cd1741a279c
quangg <quang1805kz@gmail.com>
2024-04-03 15:17:40 +0700

    giao diện : comment, one-comment, đọc truyện, viết truyện

---
+++ app/src/main/java/com/example/happy_read/activity/ConTent.java
@@ -0,0 +1,17 @@
+package com.example.happy_read.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.happy_read.R;
+
+public class ConTent extends AppCompatActivity {
+    @Override
+    protected void onCreate (Bundle comment) {
+        super.onCreate(comment);
+        setContentView(R.layout.activity_content);
+
+    }
+
+}
